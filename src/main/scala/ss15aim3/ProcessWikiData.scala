0f6dcbce03e4ee1675a88a9f2d764e5b9e7a2a50
Vassil <vsldimov@gmail.com>
2015-07-07 20:54:21 +0200

    new brunch with editFileFrequencaPart2 created

--- src/main/scala/ss15aim3/ProcessWikiData.scala
+++ src/main/scala/ss15aim3/ProcessWikiData.scala
@@ -56,62 +56,11 @@ object ProcessWikiData {
     // Assure the data has the right format (WRITE ABOUT THAT IN PAPER)
     val editsBadDataCleared = edits.filter(_.split(platfIndepNewLine)(0).split(" ").length > 5)
 
-    val userCategoryTuples = createUserCategoryTuples(editsBadDataCleared)
-      // remove duplicate categories for user
-      .distinct(0, 1)
-
-    // Use that if you need all categories of user
-    /*
-    val allCategoriesForUser = userCategoryTuples
-      // Group by user
-      .groupBy(0)
-      .reduce((t1, t2) => (t1._1, t1._2 + " " + t2._2))
-    */
-
-    // We don't need to write that
-    //allCategoriesForUser.writeAsText(outputFilePath + "/categoriesPerEditor", WriteMode.OVERWRITE)
-
-    val countCategoriesPerEditor = userCategoryTuples
-      .map(userWithCategory => (userWithCategory._1, 1))
-      .groupBy(0)
-      .sum(1)
-
-    val countAuthors = countCategoriesPerEditor.map(t => 1).reduce(_+_)
-
-    val partAuthorsWith3orLessCtgs = countCategoriesPerEditor
-      // get authors with 3 or less categories
-      .filter(_._2 <= 3)
-      // count them
-      .map(t => 1).reduce(_ + _)
-      // cross with authors count
-      .crossWithTiny(countAuthors)
-      // calculate result
-      .map(t => (t._1.toDouble / t._2, 1 - (t._1.toDouble / t._2)))
-
-
-    partAuthorsWith3orLessCtgs.writeAsText(outputFilePath + "/editUsersByCategory", WriteMode.OVERWRITE)
-
-
-
-    val categoriesNotSorted = userCategoryTuples
-      // get only the categories
-      .map(t => (t._2, 1))
-      // group by categories
-      .groupBy(0)
-      .reduce((t1, t2) => (t1._1, t1._2 + t2._2))
-
-    val categories = categoriesNotSorted
-      .sortPartition(1, Order.DESCENDING)
-      .setParallelism(1)
-      .first(20)
-
 
     //////////////////////////////////////////// END CATEGORIES ///////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     val editsFirstLine = editsBadDataCleared.map(t => t.split(platfIndepNewLine)(0))
-    val editsFirstLineNoAnonym = editsFirstLine.filter(!_.split(" ")(5).startsWith("ip:"))
-
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////// DOCID PART //////////////////////////////////////////////////
@@ -126,8 +75,6 @@ object ProcessWikiData {
       .sortPartition(1, Order.DESCENDING)
       .setParallelism(1)
       .first(20)
-
-
 
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -155,108 +102,8 @@ object ProcessWikiData {
       .groupBy(0)
       .sum(1)
 
-    categories.writeAsText(outputFilePath + "/editFileFrequencyPart3", WriteMode.OVERWRITE)
-    editsByDocId.writeAsText(outputFilePath + "/editFileFrequency", WriteMode.OVERWRITE)
     countCatsOfTop20Docs.writeAsText(outputFilePath + "/editFileFrequencyPart2", WriteMode.OVERWRITE)
     //////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    ///////////////////////////// END DOCID PART ///////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////////
-
-
-    ////////////////////////////////////////////////////////////////////////////////////////
-    ///////////////////////////// START TIMESTAMP PART /////////////////////////////////////
-
-
-     val editsByCategoryType = editsBadDataCleared
-       // get only data which contains "politic" in categories
-       .filter(_.split(newLine)(1).contains("politic"))
-       .map(
-         new MapFunction[String, (String, Int)]() {
-
-           def map(in: String): (String, Int) = {
-
-             val firstLine = in.split(newLine)(0)
-             val timestamp = firstLine.split(" ")(4)
-             val timeIndex = timestamp.indexOf("T")
-
-             // Remove the hours minutes and seconds of the date
-             (timestamp.dropRight(timeIndex), 1)
-           }
-         }
-       )
-       .groupBy(0)
-       .sum(1)
-
-    editsByCategoryType.writeAsText(outputFilePath + "/categoryEditTime/politics", WriteMode.OVERWRITE)
-
-
-
-    ///////////////////////////// END TIMESTAMP PART ///////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////////
-
-
-    val authors = editsFirstLine.map(t => t.split(" ")(5))
-
-    val countsEditsPerUser = authors
-      // Filter the anonymous users
-      .filter(!_.startsWith("ip:"))
-      .map(edit => (edit, 1)).groupBy(0)
-      .reduce((t1, t2) => (t1._1, t1._2 + t2._2))
-
-    val top10Users = countsEditsPerUser
-      .sortPartition(1, Order.DESCENDING)
-      .setParallelism(1)
-      .first(20)
-
-    //top10Users.writeAsText(outputFilePath + "/top10UsersByCount", WriteMode.OVERWRITE)
-
-    val countUsers = countsEditsPerUser.map(_ => 1).reduce(_+_)
-
-    val sumEditsByUser = countsEditsPerUser
-      .map(edit => edit._2)
-      .reduce(_ + _)
-
-    val avgEditsCountPerUser = countUsers
-      .crossWithTiny(sumEditsByUser)
-      // edits / countUsers
-      .map(in => in._2.toDouble / in._1)
-
-    val editsByAnonymous = authors
-      .filter(_.startsWith("ip:"))
-      // Get the count
-      .map(_ => 1).reduce(_ + _)
-
-    val editsTotal = authors
-      .map(_ => 1).reduce(_ + _)
-
-    val percentageAnonymous = editsByAnonymous
-      .cross(editsTotal)
-      .map(in => (2015, in._1.toDouble / in._2, 1 - (in._1.toDouble / in._2)))
-
-    //percentageAnonymous.writeAsCsv(outputFilePath + "/editionByAnonyms", csvRowDelimeter, csvFieldDelimeter, WriteMode.OVERWRITE)
-
-
-    //countsEditsPerUser.writeAsText(outputFilePath + "/editsPerUser", WriteMode.OVERWRITE)
-
-
-    val countPerUserGroup = calculateUserGroups(countsEditsPerUser)
-
-    val outputCountPerUserGroupAndAvg = countPerUserGroup
-      .crossWithTiny(avgEditsCountPerUser)
-      .map(t => (2015, t._1._1, t._1._2, t._1._3, t._1._4, t._1._5, t._1._6, t._1._7, t._2))
-
-    //outputCountPerUserGroupAndAvg.writeAsCsv(outputFilePath + "/editsUsersByDocCount", csvRowDelimeter, csvFieldDelimeter, WriteMode.OVERWRITE)
-
-    //////////////////////////////////////////////////////////////////////
-
-    generateDataByDate(editsFirstLineNoAnonym, top10Users)
-
-
-    top10Users.writeAsText(outputFilePath + "/top10UsersByCount", WriteMode.OVERWRITE)
-    percentageAnonymous.writeAsCsv(outputFilePath + "/editionByAnonyms", csvRowDelimeter, csvFieldDelimeter, WriteMode.OVERWRITE)
-    outputCountPerUserGroupAndAvg.writeAsCsv(outputFilePath + "/editsUsersByDocCount", csvRowDelimeter, csvFieldDelimeter, WriteMode.OVERWRITE)
-    //////////////////////////////////////////////////////////////////////
 
     env.execute("Scala AssociationRule Example")
   }
@@ -307,79 +154,6 @@ object ProcessWikiData {
     })
   }
 
-
-  def calculateUserGroups(countsEditsPerUser: DataSet[(String, Int)]): DataSet[(Int, Int, Int, Int, Int, Int, Int)] = {
-
-    val countPerUserGroup = countsEditsPerUser.map(new MapFunction[(String, Int), (Int, Int, Int, Int, Int, Int, Int)]() {
-      def map(in: (String, Int)): (Int, Int, Int, Int, Int, Int, Int) = {
-        var isIn0_5Group = 0
-        var isIn6_10Group = 0
-        var isIn11_15Group = 0
-        var isIn16_20Group = 0
-        var isIn21_25Group = 0
-        var isIn26_30Group = 0
-        var isIn30PGroup = 0
-        if (in._2 <= 5) {
-          isIn0_5Group = 1
-        } else if (in._2 <= 10) {
-          isIn6_10Group = 1
-        } else if (in._2 <= 15) {
-          isIn11_15Group = 1
-        } else if (in._2 <= 20) {
-          isIn16_20Group = 1
-        } else if (in._2 <= 25) {
-          isIn21_25Group = 1
-        } else if (in._2 <= 30) {
-          isIn26_30Group = 1
-        } else {
-          isIn30PGroup = 1
-        }
-        (isIn0_5Group, isIn6_10Group, isIn11_15Group, isIn16_20Group, isIn21_25Group, isIn26_30Group, isIn30PGroup)
-      }
-    })
-      // calculate the count in each group
-      .reduce((t1, t2) => (t1._1 + t2._1, t1._2 + t2._2, t1._3 + t2._3, t1._4 + t2._4, t1._5 + t2._5, t1._6 + t2._6, t1._7 + t2._7))
-
-    countPerUserGroup
-  }
-
-  def generateDataByDate(editsFirstLine: DataSet[String], top10User: DataSet[(String, Int)]) = {
-
-    val userEditsByDate = editsFirstLine
-      // get all edit timestamps
-      .map(
-
-        new MapFunction[String, (String, String, Int)]() {
-          def map(in: String): (String, String, Int) = {
-
-            val user = in.split(" ")(5)
-            val timestamp = in.split(" ")(4)
-            val timeIndex = timestamp.indexOf("T")
-
-            // Remove the hours minutes and seconds of the date
-            (timestamp.dropRight(timeIndex), user, 1)
-          }
-        })
-
-
-    val countEditsByDate = userEditsByDate
-      .map(t => (t._1, t._3))
-      .groupBy(0)
-      .reduce((t1, t2) => (t1._1, t1._2 + t2._2))
-
-    countEditsByDate.writeAsCsv(outputFilePath + "/userEditTimeCount", csvRowDelimeter, csvFieldDelimeter, WriteMode.OVERWRITE)
-
-
-    val countEditsByUserDate = userEditsByDate
-      .join(top10User)
-      .where(1)
-      .equalTo(0)
-      .map(_._1)
-      .groupBy(0,1)
-      .sum(2)
-
-    countEditsByUserDate.writeAsCsv(outputFilePath + "/userEditTime", csvRowDelimeter, csvFieldDelimeter, WriteMode.OVERWRITE)
-  }
 
 }
 
